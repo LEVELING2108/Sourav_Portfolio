b241c60607cd6357aec65ed6f25940d45a5a6428
LEVELING2108 <24f3004824@ds.study.iitm.ac.in>
2026-09-06 21:23:21 +0530

    feat(preloader): add Concept 2 Minimalist Studio Counter and Copper Trace preloader

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Preloader from "./components/Preloader";
+import PreloaderStudio from "./components/PreloaderStudio";
 import Nav from "./components/Nav";
 import Hero from "./components/Hero";
 import Projects from "./components/Projects";
@@ -11,7 +11,7 @@ import CommandPalette from "./components/CommandPalette";
 export default function Home() {
   return (
     <>
-      <Preloader />
+      <PreloaderStudio />
       <Nav />
       <main className="flex-1">
         <Hero />
